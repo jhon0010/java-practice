88bcb36a8f0c5d0707583180d8cf27d337a59c2c
Jhon Lotero <jhoning.soft@gmail.com>
2023-10-30 13:56:07 -0500

    * Collections - SortedMap - TreeMap.

---
+++ src/main/java/practice/collections/maps/TreeMapExample.java
@@ -0,0 +1,40 @@
+package practice.collections.maps;
+
+import java.util.SortedMap;
+
+/**
+ * TreeMap is a Red-Black tree based NavigableMap implementation.
+ * The map is sorted according to the natural ordering of its keys, or by a Comparator provided at map creation time,
+ * depending on which constructor is used.
+ * This implementation provides guaranteed log(n) time cost for the containsKey, get, put and remove operations.
+ *
+ */
+public class TreeMapExample {
+
+    public static void main(String[] args) {
+
+        SortedMap<String, Integer> ageMap = new java.util.TreeMap<>();
+        ageMap.put("Zuly", 20);
+        ageMap.put("Jhon", 30);
+        ageMap.put("Alice", 31);
+        ageMap.put("Bob", 32);
+        ageMap.put("Charlie", 33);
+
+        for(String key : ageMap.keySet()) {
+            System.out.println("Key: " + key + " Value: " + ageMap.get(key));
+        }
+
+        // Retrieve a submap for keys within the range ["Alice", "Charlie"]
+        SortedMap<String, Integer> submap = ageMap.subMap("Alice", "Charlie");
+        System.out.println(submap);
+
+        // headMap and tailMap are similar to subMap, except that they include the start and end keys
+        SortedMap<String, Integer> headMap = ageMap.headMap("Charlie");
+        System.out.println(headMap);
+
+        SortedMap<String, Integer> tailMap = ageMap.tailMap("Charlie");
+        System.out.println(tailMap);
+
+
+    }
+}
